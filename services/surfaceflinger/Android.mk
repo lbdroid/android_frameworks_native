e246c1d5a829e60db611d68ed27f7e5a8250a560
lbdroid <lbdroid@users.noreply.github.com>
2014-12-10 11:01:34 -0500

    Fix path to display-caf

--- services/surfaceflinger/Android.mk
+++ services/surfaceflinger/Android.mk
@@ -97,8 +97,8 @@ LOCAL_SHARED_LIBRARIES := \
 	libpowermanager
 
 ifeq ($(TARGET_USES_QCOM_BSP), true)
-    LOCAL_C_INCLUDES += hardware/qcom/display/libgralloc
-    LOCAL_C_INCLUDES += hardware/qcom/display/libqdutils
+    LOCAL_C_INCLUDES += hardware/qcom/display-caf/libgralloc
+    LOCAL_C_INCLUDES += hardware/qcom/display-caf/libqdutils
     LOCAL_SHARED_LIBRARIES += libqdutils
     LOCAL_CFLAGS += -DQCOM_BSP
 endif
